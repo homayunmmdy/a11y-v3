a07e5aa72d0b4990558450bdbbdf9478d9dafaa6
Homayoun <blodbyte.c@gmail.com>
2024-01-25 12:31:03 +0330

    Create pages

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 const Navbar = () => {
   return (
@@ -11,10 +12,10 @@ const Navbar = () => {
             <img
               src='/logo.png'
               className="h-8"
-              alt="yum logo"
+              alt="My Apo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Yum
+              My App
             </span>
           </a>
           <div className="flex md:order-2">
@@ -124,29 +125,28 @@ const Navbar = () => {
             </div>
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                  aria-current="page"
+                <Link
+                  href="/"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/about"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   About
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/login"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Login
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
