6e6e0a1baa7136c278f815e9a1eb31300c357fb2
Homayoun <homayoun763@gmail.com>
2024-02-02 23:52:09 +0330

    Categories section

--- app/[locale]/blogs/page.tsx
+++ app/[locale]/blogs/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { DATAPOST } from "@/DATA";
 import Image from 'next/image';
+import Category from './components/category/Category';
 
 const Blogs = () => {
   return (
     <div>
+      <Category />
         {DATAPOST.map(item => (
             <div key={item.id}>
                 <Image src="/images/1.png" width={300} height={300} title={item.title} alt={item.slug}/>
