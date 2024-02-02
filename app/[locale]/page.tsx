062f5e1d2ef1c81a4322a801812187b940d548c7
Homayoun <homayoun763@gmail.com>
2024-02-02 14:54:25 +0330

    create newletter section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/hero/Hero'
+import Newsletter from './components/newsletter/Newsletter'
 
 const Home = () => {
   return (
     <div className='bg-base-100'>
       <Hero />
+      <Newsletter />
     </div>
   )
 }
