cc0a55bdf690f5f3b76aff0174781c4b59ea1f7f
Homayoun <homayoun763@gmail.com>
2024-03-06 02:19:19 +0330

    Focus on MVP

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const withNextIntl = createNextIntlPlugin();
- 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                hostname: "media.licdn.com",
-                protocol: "https",
-            },
-        ],
-    },
-};
- 
-export default withNextIntl(nextConfig);
+const nextConfig = {};
+
+export default nextConfig;
--- types/collection.ts
+++
@@ -1,8 +0,0 @@
-export interface Post {
-  id: string;
-  title: string;
-  description: string;
-  slug: string;
-  image: string;
-  body: string;  
-}
